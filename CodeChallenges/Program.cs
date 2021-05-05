d1ea22fd658355a2711c66681231605857666fec
danisylvester <maria.sylvester10@gmail.com>
2021-05-05 12:15:43 -0500

    Added method to get the longest substring of a string.

--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -8,32 +8,62 @@ namespace CodeChallenges
     {
         static void Main(string[] args)
         {
-            string[] optionsArr = new string[] {"cat", "hat", "dog", "mat"};
-            List<string> results = Unscramble("tac", optionsArr);
-            
-            foreach(string el in results)
-            {
-                Console.WriteLine(el);
-            }
+            Console.WriteLine(GetLongestSubstring("abacdaacdesf"));
         }
 
         static List<string> Unscramble(string input, string[] options)
         {
             string sortedInput = new string(input.OrderBy(c => c).ToArray());
-;
             List<string> result = new List<string>();
 
-            for (int i = 0; i < options.Length; i++)
+            for(int i = 0; i < options.Length; i++)
             {
-                string sortedListItem = new string(options[i].OrderBy(c => c).ToArray());
+                string sortedEl = new string(options[i].OrderBy(c => c).ToArray());
                 
-                if(sortedInput == sortedListItem)
+                if(sortedEl == sortedInput)
                 {
                     result.Add(options[i]);
                 }
             }
-
             return result;
+        }
+
+        static char? GetDuplicate(List<char> arr)
+        {
+            var duplicates = arr.GroupBy(c => c)
+                                .Where(g => g.Count() > 1)
+                                .Select(g => g.Key)
+                                .ToList();
+            char? result = null;
+            return duplicates.Count() == 0 ? result : duplicates[0];
+        }
+
+        static int GetLongestSubstring(string input)
+        {
+            char[] inputArr = input.ToCharArray();
+            int maxLength = 0;
+            List<char> temp = new List<char>();
+            int deleteNum;
+
+            for(int i = 0; i < inputArr.Length; i++)
+            {
+                temp.Add(inputArr[i]);
+                char? duplicate = GetDuplicate(temp);
+
+                if(duplicate.GetValueOrDefault() == 0)
+                {
+                    if(maxLength < temp.Count())
+                    {
+                        maxLength = temp.Count();
+                    }
+                } else
+                {
+                    // abcdb
+                    deleteNum = temp.IndexOf(duplicate.GetValueOrDefault()) + 1;
+                    temp.RemoveRange(0, deleteNum);
+                }
+            }
+            return maxLength;
         }
     }
 }
