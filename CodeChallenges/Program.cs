bbe067d8ea17d546a3dce35e3401201246f5e36e
danisylvester <maria.sylvester10@gmail.com>
2021-05-04 22:25:22 -0500

    Unscramble challenge completed. Created a method that unscrambles an input by comparing it to elements in an array.

---
+++ CodeChallenges/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] optionsArr = new string[] {"cat", "hat", "dog", "mat"};
+            List<string> results = Unscramble("tac", optionsArr);
+            
+            foreach(string el in results)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+        static List<string> Unscramble(string input, string[] options)
+        {
+            string sortedInput = new string(input.OrderBy(c => c).ToArray());
+;
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                string sortedListItem = new string(options[i].OrderBy(c => c).ToArray());
+                
+                if(sortedInput == sortedListItem)
+                {
+                    result.Add(options[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
